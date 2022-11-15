c5973d71f0598ae8883d0ba1f57885fe63591bc3
Johany Quintero Toro <johany.quinterot@perficient.com>
2022-11-15 16:56:43 -0500

    read multiple selectors at the same time

--- tests/firstTest.spec.js
+++ tests/firstTest.spec.js
@@ -6,18 +6,25 @@ test('First test case', async ({page}) => {
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     const userName = page.locator('#username');
     const signInBtn =  page.locator('#signInBtn');
+    const cardTittles = page.locator('.card-body a');
+
     console.log(await page.title())
     await page.locator('#username').type('johany');
     await page.locator('[type=password]').type('learning');
     await signInBtn.click();
     //Playwright will wait until it appears
     const blockText = await page.locator(`[style*='block']`).textContent();
+
     await expect(page.locator(`[style*='block']`)).toContainText('Incorrect username/password')
     await userName.fill('');
     await userName.fill('rahulshettyacademy');
     await signInBtn.click();
-    console.log( await page.locator('.card-body a').first().textContent());
-    console.log( await page.locator('.card-body a').nth(1).textContent());
+
+    console.log(cardTittles.first().textContent());
+    console.log(cardTittles.nth(1).textContent());
+
+    const allTittles = await cardTittles.allTextContents();
+    console.log(allTittles);
 });
 
 test('Second test case', async ({page}) => {
